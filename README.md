2f1e4c2f524203065b914e9722cf339ab6bd2998
playmono <adrgranado@gmail.com>
2024-05-29 21:38:26 +0200

    feat: add empty project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # benchmark-playground
 Benchmark playground
+
+# Resources
+- https://spring.io/guides/topicals/spring-boot-docker
+- https://spring.io/guides/tutorials/spring-boot-kotlin
---
+++ backoffice-kotlin/service/src/test/kotlin/com/backoffice/kotlin/BackofficeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backoffice.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackofficeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
